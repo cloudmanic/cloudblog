d4461fea22800af6cb3aa5931bc3cffd699f1766
Cloudmanic Labs, LLC <github@cloudmanic.com>
2011-07-31 11:53:01 -0700

    Added get by name to cbblocks model

--- models/cbblocks_model.php
+++ models/cbblocks_model.php
@@ -10,6 +10,15 @@ class cbblocks_model extends cbshared_model
 		parent::__construct();
 		$this->_table = 'Blocks';	
 	}
+	
+	//
+	// Return the contents of a block by the block name.
+	//
+	function get_by_name($name)
+	{
+ 		$this->db->where($this->_table . 'Name', $name);
+		return $this->db->get($this->_prefix . $this->_table)->row_array();
+	} 
 }
 
 /* End File */
